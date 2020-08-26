aa8ad657e1fdf240c05213217ba5f0d8223ec89d
Daniel <ngo8@stolaf.edu>
2020-08-26 17:49:38 -0500

    lab2: complete

---
+++ lab2/sqrt0.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  float x;
+  x = 3.28;
+  printf("The square root of %g is %f\n", x, sqrt(x));
+}
---
+++ lab2/tests2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    float v = -3;
+    if (v == 0)
+        printf("v is equal to 0\n");
+    else if (v > 0)
+        printf("v is positive\n");
+    else
+        printf("v is negative\n");
+
+    float f = 4.3;
+    if (f == (int)f)
+        printf("f is an integer\n");
+    else
+        printf("f is a float\n");
+
+    float i = 17;
+    float x = sqrt(i);
+    if (x == (int)x)
+        printf("i is a square number\n");
+    else
+        printf("i is not a square number\n");
+    
+}

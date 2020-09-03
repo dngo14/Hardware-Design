27f0e17863063d8b5743fefff8c3e74d7ddb22c5
Daniel <ngo8@stolaf.edu>
2020-09-03 15:36:45 -0500

    lab6B: max.c

---
+++ lab6/max.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+float max(float x, float y) {
+  if (x < y)
+    return y;
+  else
+    return x;
+} 
+
+int main() {
+    printf("Enter two floats\n");
+    float x, y, z;
+    scanf("%f%f", &x, &y);
+    z = max(x, y);
+    printf("The max is %f\n", z);
+}

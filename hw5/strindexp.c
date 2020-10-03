586876841a079449ad5e461658fc44f02e6c4f25
Daniel <ngo8@stolaf.edu>
2020-10-03 18:19:52 -0500

    hw5: Part B2 - complete

---
+++ hw5/strindexp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int strindex(char *arg1, char arg2) {
+  int num = 0;
+  while(*arg1 != '\0' & *arg1 != arg2) {
+      num++;
+      arg1++;
+  }
+    if(*arg1 == arg2) {
+      return num;
+    }
+    else {
+      return -1;
+    }
+}
+  
+int main() {
+  int x;
+  x = strindex("The end", 'e');
+  int y;
+  y = strindex("The end", 'a');
+  printf("The return value from strindex(\"The end\", 'e') is %i\n", x);
+  printf("The return value from strindex(\"The end\", 'a') is %i\n", y);
+}
+
+

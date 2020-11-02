2bd65105be1db940255e8b08e403d278e9dc5d17
Daniel <ngo8@stolaf.edu>
2020-11-02 17:51:05 -0600

    Lab24 B1 - complete

--- lab24/isneg.s
+++ lab24/isneg.s
@@ -11,37 +11,53 @@ result:
 	.text
 	.align	2
 isneg:
-	push	{fp, lr}
+	push	{fp, lr}	@stack setup
 	add	fp, sp, #4
-	sub	sp, sp, #8
-	str	r0, [fp, #-8]
-	mov	r0, #1
-	sub	sp, fp, #4
-	pop	{fp, pc}
-	.text
-	.align	2
-	.global main
+	sub	sp, sp, #8	
+	str	r0, [fp, #-8]	@store variable
+	b	guard		@branch to guard
+guard:
+	ldr	r0, [fp, #-8]	@load stored variable
+	mov	r1, #0
+	cmp	r0, r1		@cmp the variable to 0
+	ble	else		@branch if less than 0
+	mov	r0, #1		@if not less than 0 return 1/store it
+	str	r0, [fp, #-12]
+	b	next		@branch to next
+
+else:	
+	mov	r0, #0		@return 0/store it
+	str	r0, [fp, #-12]
+next:
+	ldr     r0, [fp, #-12]	@load the stored 1 or 0 and puts it into r0
+	sub     sp, fp, #4
+        pop     {fp, pc}	@stack teardown
+        .text
+        .align  2
+        .global main
+
+	
 main:
-	push	{fp, lr}
+	push	{fp, lr}	@stack setup
 	add	fp, sp, #4
 	sub	sp, sp, #8
-	ldr	r0, promptp
+	ldr	r0, promptp	@print prompt
 	bl	printf
-	ldr	r0, inputp
+	ldr	r0, inputp	@get input
 	sub	r1, fp, #8
 	bl	__isoc99_scanf
-	ldr	r0, [fp, #-8]
+	ldr	r0, [fp, #-8]	@call the function isneg
 	bl	isneg
-	str	r0, [fp, #-12]
-	ldr	r0, resultp
+	str	r0, [fp, #-12]	@store return value
+	ldr	r0, resultp	
 	ldr	r1, [fp, #-8]
-	ldr	r2, [fp, #-12]
+	ldr	r2, [fp, #-12]	@print the result
 	bl	printf
-	mov	r0, #0
+	mov	r0, #0		@stack teardown
 	sub	sp, fp, #4
 	pop	{fp, pc}
 	.align	2
-promptp:	.word	prompt
+promptp:	.word	prompt		@pointers
 inputp:		.word	input
 resultp:	.word	result
 	

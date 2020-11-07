c377f236e4a06a2e60316719376bf9711d7c6470
Daniel <ngo8@stolaf.edu>
2020-11-06 21:17:49 -0600

    stage1.B - step 3 of incremental development - gettrans

--- proj/stage1.s
+++ proj/stage1.s
@@ -3,20 +3,58 @@
 prompt:
 	.asciz	"Enter a line of input: "	
 	.align	2
-result:
+resultchar:
+	.asciz	"The characters with index 0 and 2 are %c and %c\n"
+	.align	2
+resultstring:
 	.asciz "%s\n"
 	.text
-	.align2
+	.align	2
 
 gettrans:
 	push	{fp, lr}
 	add	fp, sp, #4
-	sub	sp, sp, #8
+	sub	sp, sp, #16
+	str	r0, [fp, #-8]
 	
+	ldr	r0, [fp, #-8]
+	mov	r1, #1
+	bl	get_byte
+	cmp	r0, #''
+	beq	else
+
+	ldr	r0, [fp, #-8]
+	mov	r1, #3
+	bl	get_byte
+	cmp	r0, #'\n'
+	beq	else
 	
-	.text
-	.align 	2
-	.global main
+	ldr     r0, [fp, #-8]
+	mov     r1, #4
+	bl	get_byte
+	cmp	r0, #0
+	beq	else
+
+	ldr	r0, [fp, #-8]
+	mov	r1, #0
+	bl	get_byte
+	mov	r4, r0
+
+	ldr	r0, [fp, #-8]
+	mov	r1, #2
+	bl	get_byte
+	mov	r5, r0
+
+	mov     r0, #1
+	b 	next
+else:
+        mov     r0, #0	
+next:
+	sub	sp, fp, #4
+	pop	{fp, pc}
+        .text
+        .align  2
+        .global main	
 main:
 	push	{fp, lr}
 	add	fp, sp, #4
@@ -28,8 +66,17 @@ main:
 	mov	r1, #100
 	bl	get_line
 	str	r0, [fp, #-8]
+
+	ldr	r0, buffp
+	bl	gettrans
 	
-	ldr	r1, resultp
+	ldr	r0, resultcharp
+	mov	r1, r4
+	mov	r2, r5
+        bl      printf
+
+	
+	ldr	r1, resultstringp
 	ldr	r0, buffp
 	bl	printf
 	mov	r0, #0
@@ -42,7 +89,8 @@ main:
 	
 buffp:		.word	buff
 promptp:	.word	prompt
-resultp:	.word	result
+resultstringp:	.word	resultstring
+resultcharp:	.word	resultchar
 	.data
 	.align	2
 buff:	.skip	100
